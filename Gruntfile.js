3fe1a0856be8af1a0913ef4f7a710ee2382d813f
Vladimir Strackovski <vlado@nv3.org>
2015-05-14 15:34:03 +0200

    Unify project structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        distFolder: 'assets/build',
+        distFolder: 'release/assets',
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             files: {
-                src: 'assets/scripts/*.js',
-                dest: 'assets/build/',
+                src: 'source/assets/scripts/*.js',
+                dest: 'release/assets/scripts/',
                 expand: true,
                 flatten: true
             }
@@ -18,16 +18,39 @@ module.exports = function(grunt) {
             target: {
                 files: [{
                     expand: true,
-                    cwd: 'assets/styles',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'assets/build',
-                    ext: '.min.css'
+                    cwd: 'source/assets/styles',
+                    src: ['*.css'],
+                    dest: 'release/assets/styles'
                 }]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: false,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'release/index.html': 'source/index.html',
+                    'release/docs.html': 'source/docs.html',
+                    'release/download.html': 'source/download.html',
+                    'release/404.html': 'source/404.html'
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: 'source/assets/fonts/', src: ['**'], dest: 'release/assets/fonts/'},
+                    {expand: true, cwd: 'source/assets/images/', src: ['**'], dest: 'release/assets/images/'}
+                ]
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('default', ['uglify', 'cssmin']);
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin', 'copy']);
 };
